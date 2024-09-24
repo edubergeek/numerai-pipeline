2f8457d9c108f7b3571d2a1bb0198e48b11bbcbe
Curt Dodds <curt.dodds@gmail.com>
2024-09-24 15:58:46 +0000

    Support ATLAS data release
    Run MLP and CNN models on ATLAS data release

--- prepare.sh
+++ prepare.sh
@@ -7,10 +7,11 @@ python download.py --meta
 python download.py --features
 
 # Extract and Transform to sharded TFRecord files in ./data
-python etl.py --train --split 0.04 --shard 1024
+python etl.py --train --split_valid --all --split 0.20 --shard 4096
 
 # Create a baseline model for training
 python naiMLP.py
+python naiCNN.py
 
 # Train the model(s)
 bash train.sh
