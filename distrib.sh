b9535aa9582faa8d20f56a8c5b9e35936f30c0a8
Curt Dodds <curt.dodds@gmail.com>
2025-11-15 23:09:09 +0000

    Updated gitignore and distrib.sh
    gitignore: ignore directories starting with 20 and parquet files
    distrib.sh:  handle all 11 bins not just 0-90%

--- distrib.sh
+++ distrib.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 file=$1*.csv
-for d in 1 2 3 4 5 6 7 8 9
+for d in 0 1 2 3 4 5 6 7 8 9
 do
 n=`grep "0\.$d" $file|wc -l`
-printf "%d %d\n" $d $n
+printf "%d0 %d\n" $d $n
 done
+n=`grep "1\.0" $file|wc -l`
+printf "%d %d\n" 100 $n
