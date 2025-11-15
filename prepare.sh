ac8dea2f6d35915fe564fafd93fc6cf645082f15
Curt Dodds <curt.dodds@gmail.com>
2025-11-15 23:21:36 +0000

    Use FORCE option from env variable
    Control the new --force option using an environment variable
    Forces overwrite of existing parquet files
    Useful to download updated validation set

--- prepare.sh
+++ prepare.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# To force new downloads set the environment variable FORCE to --force
 # Download Numerai dataset if not already present
-python download.py --live
-python download.py --train
-python download.py --meta
-python download.py --features
+#python download.py --live ${FORCE}
+python download.py --train ${FORCE}
+python download.py --meta ${FORCE}
+python download.py --features ${FORCE}
 
 # Extract and Transform to sharded TFRecord files in ./data
 python etl.py --train --split_valid --all --split 0.20 --shard 4096
+#python etl.py --train --split_valid --all --split 0.02 --shard 4096 --features features.json --dir ./data/balance --balance
+
 
 # Create a baseline model for training
 python naiMLP.py
